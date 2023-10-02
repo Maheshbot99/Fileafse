6b2048605fb6333a352d5ae7e63c1228481c3246
Maheshbot99 <115773959+Maheshbot99@users.noreply.github.com>
2023-10-02 13:30:23 +0530

    Update stream.py

--- Dxbots/bot/plugins/stream.py
+++ Dxbots/bot/plugins/stream.py
@@ -66,7 +66,7 @@ async def private_receive_handler(c: Client, m: Message):
             if user.status == "kicked":
                 await c.send_message(
                     chat_id=m.chat.id,
-                    text="You are banned!\n\n  **Cᴏɴᴛᴀᴄᴛ Support [Support](https://t.me/crazysupportz) They Wɪʟʟ Hᴇʟᴘ Yᴏᴜ**",
+                    text="You are banned!\n\n  **Cᴏɴᴛᴀᴄᴛ Support [Support](https://t.me/CrazyXBoTsBot?start) They Wɪʟʟ Hᴇʟᴘ Yᴏᴜ**",
                     
                     disable_web_page_preview=True
                 )
